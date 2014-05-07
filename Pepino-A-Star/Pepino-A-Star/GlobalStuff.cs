520bbf2cbab178eca6065241ca9e95b768a69cf3
edu444 <edu444@gmail.com>
2014-05-07 12:10:05 +0100

    Created program core

---
+++ Pepino-A-Star/Pepino-A-Star/GlobalStuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepino_A_Star
+{
+    public static class GlobalStuff
+    {
+
+        public static PGMExtremeLoader _loadMenu;
+        public static PathFinderMenu _pathMenu;
+
+        public static Bitmap _image;
+
+        public static bool IsImageLoaded;
+
+    }
+}
